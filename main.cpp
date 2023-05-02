ef6f44a8f585a9c6eb9419c277a8fad06ce60a7d
Peter_Spurny <peter.spurny@tul.cz>
2023-05-02 19:55:14 +0200

    libcurl custom email with verification code

--- main.cpp
+++ main.cpp
@@ -17,14 +17,79 @@ struct Credentials {
     string password;
 };
 
-int generateRandomNumber() {
-    srand(time(NULL));  // Seed the random number generator with the current time
-    int random_number = rand() % 9000 + 1000;  // Generate a random number between 1000 and 9999
-    return random_number;
+#define FROM    "peter.spurny@tul.cz"
+#define TO		"peter.spurny@outlook.com"
+#define USERNAME "peter.spurny"
+#define PASSWORD ""
+#define MAILTO "peter.spurny@outlook.com"
+#define MAILFROM "peter.spurny@tul.cz"
+#define SMTP "smtp.tul.cz:587"
+
+
+struct ReadData
+{
+    explicit ReadData(const char* str)
+    {
+        source = str;
+        size = strlen(str);
+    }
+
+    const char* source;
+    size_t size;
+};
+
+size_t read_function(char* buffer, size_t size, size_t nitems, ReadData* data)
+{
+    size_t len = size * nitems;
+    if (len > data->size) { len = data->size; }
+    memcpy(buffer, data->source, len);
+    data->source += len;
+    data->size -= len;
+    return len;
 }
+void sendEmail(const std::string& recipient, int& code)
+{
+    CURL* curl = curl_easy_init();
+    if (!curl)
+    {
+        fprintf(stderr, "curl_easy_init failed\n");
 
+    }
 
+    curl_easy_setopt(curl, CURLOPT_USERNAME, USERNAME);
+    curl_easy_setopt(curl, CURLOPT_PASSWORD, PASSWORD);
+    curl_easy_setopt(curl, CURLOPT_URL, SMTP);
+    curl_easy_setopt(curl, CURLOPT_MAIL_FROM, MAILFROM);
 
+    struct curl_slist* rcpt = NULL;
+    rcpt = curl_slist_append(rcpt, MAILTO);
+    curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, rcpt);
+    string message = "Date: Mon, 29 Nov 2010 21:54:29 +1100\r\n"
+        "To: " + recipient + "\r\n" 
+        "From: " MAILFROM "\r\n" 
+        "Subject: Verification Code\r\n"
+        "\r\n"
+        "Code: " + to_string(code) + "\r\n" 
+        "This is a test email.\r\n";
+    ReadData data(message.data());
+    curl_easy_setopt(curl, CURLOPT_READDATA, &data);
+    curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_function);
+
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1);
+    curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_ALL);
+
+    // If your server doesn't have a proper SSL certificate:
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+
+    CURLcode res = curl_easy_perform(curl);
+    if (res != CURLE_OK)
+    {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+            curl_easy_strerror(res));
+        curl_easy_cleanup(curl);
+    }
+}
 
 int main()
 {
@@ -82,8 +147,9 @@ int main()
 
     // Define a welcome page to redirect the user to after a successful login
     CROW_ROUTE(app, "/welcome")([]() {
-        //sendEmail("hikawa.renko99@gmail.com");
-        sendPythonEmail("peter.spurny@outlook.com", to_string(generateRandomNumber()));
+        int code = generateRandomNumber();
+        sendEmail("peter.spurny@outlook.com", code);
+        //sendPythonEmail("peter.spurny@outlook.com", to_string(generateRandomNumber()));
         return "Welcome!";
         });
 
