70a1944fe93a4ff5b65a48c8106548302924219d
Peter_Spurny <peter.spurny@tul.cz>
2023-05-06 22:23:55 +0200

    2fa verified

--- main.cpp
+++ main.cpp
@@ -15,78 +15,13 @@ using namespace std;
 using json = nlohmann::json;
 
 struct Credentials {
-    string email;
-    string password;
+    std::string email;
+    std::string password;
 };
 
 #define MAILFROM "peter.spurny@tul.cz"
 #define SMTP "smtp.tul.cz:587"
-
-
-struct ReadData
-{
-    explicit ReadData(const char* str)
-    {
-        source = str;
-        size = strlen(str);
-    }
-
-    const char* source;
-    size_t size;
-};
-
-size_t read_function(char* buffer, size_t size, size_t nitems, ReadData* data)
-{
-    size_t len = size * nitems;
-    if (len > data->size) { len = data->size; }
-    memcpy(buffer, data->source, len);
-    data->source += len;
-    data->size -= len;
-    return len;
-}
-void sendEmail(const std::string& recipient, int& code, string username, string password)
-{
-    CURL* curl = curl_easy_init();
-    if (!curl)
-    {
-        fprintf(stderr, "curl_easy_init failed\n");
-
-    }
-
-    curl_easy_setopt(curl, CURLOPT_USERNAME, username.c_str());
-    curl_easy_setopt(curl, CURLOPT_PASSWORD, password.c_str());
-    curl_easy_setopt(curl, CURLOPT_URL, SMTP);
-    curl_easy_setopt(curl, CURLOPT_MAIL_FROM, MAILFROM);
-
-    struct curl_slist* rcpt = NULL;
-    rcpt = curl_slist_append(rcpt, recipient.c_str());
-    curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, rcpt);
-    string message = "Date: Mon, 29 Nov 2010 21:54:29 +1100\r\n"
-        "To: " + recipient + "\r\n"
-        "From: " MAILFROM "\r\n"
-        "Subject: Verification Code\r\n"
-        "\r\n"
-        "Code: " + to_string(code) + "\r\n"
-        "This is a test email.\r\n";
-    ReadData data(message.data());
-    curl_easy_setopt(curl, CURLOPT_READDATA, &data);
-    curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_function);
-
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
-    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1);
-    curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_ALL);
-
-    // If your server doesn't have a proper SSL certificate:
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-
-    CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK)
-    {
-        fprintf(stderr, "curl_easy_perform() failed: %s\n",
-            curl_easy_strerror(res));
-        curl_easy_cleanup(curl);
-    }
-}
+#define USERSJ "users.json"
 
 int main()
 {
@@ -119,11 +54,9 @@ int main()
         json req_body = json::parse(req.body);
 
         // Get email and password from request body
-        string email = req_body["email"].get<std::string>();
-        string password = req_body["password"].get<std::string>();
-        ifstream json_file("data.json");
-        json data;
-        json_file >> data;
+        std::string email = req_body["email"].get<std::string>();
+        std::string password = req_body["password"].get<std::string>();
+        json data = parseJson(USERSJ);
 
         // Verify login credentials
         bool is_valid_login = verify_login(email, password, data);
@@ -160,10 +93,31 @@ int main()
         ([&](const crow::request& req) {
         int code = generateRandomNumber();
         json req_body = json::parse(req.body);
-        string mail = req_body["email"].get<string>();
+        std::string mail = req_body["email"].get<std::string>();
         sendEmail(mail, code, username, password);
-        return "Welcome";
+        json data = parseJson(USERSJ);
+        insertCode(mail, data, code);
+        return "2fa";
             });
+
+    CROW_ROUTE(app, "/2faCode").methods("POST"_method)([&](const crow::request& req) {
+        json req_body = json::parse(req.body);
+        std::string mail = req_body["email"].get<std::string>();
+        std::string code = req_body["code"].get<std::string>();
+        json data = parseJson(USERSJ);
+        bool isVerified = verifyCode(mail, data, code);
+        json response;
+        if (isVerified) {
+            response["success"] = true;
+            response["message"] = "Login succeeded";
+        }
+        else {
+            response["success"] = false;
+            response["message"] = "Invalid code";
+        }
+        crow::response res{ response.dump() };
+        return res;
+        });
 
 
     app.port(18080).multithreaded().run();
